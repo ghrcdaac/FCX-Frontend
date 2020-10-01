cbb11cca63af8d916a371bf46c4f358c6f229481
Ajinkya Kulkarni <akulkarni@itsc.uah.edu>
2020-10-01 12:48:37 -0500

    renamed config variables

--- src/config.js
+++ src/config.js
@@ -6,15 +6,16 @@
     In future, actual values of these variables should be directly read from environment variables and defaults should be removed.
 */
 
-const dataBaseUrl = process.env.DATA_BASE_URL || "https://ghrc-fcx-viz-output.s3-us-west-2.amazonaws.com"
-const abiBaseUrl = process.env.ABI_BASE_URL || "https://q6eymnfsd9.execute-api.us-west-2.amazonaws.com"
-const flightTrackBaseUrl = process.env.FLIGHT_TRACK_BASE_URL || "https://fcx-czml.s3.amazonaws.com"
-const mapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN || "pk.eyJ1IjoiYWppbmt5YWt1bGthcm5pIiwiYSI6ImNrYzY1NHh3bDA3cXIyenA4dDdpOGlja2UifQ.g4r3pjugLK8yC2mkQmJr4w"
-const mapboxStyleId = process.env.MAPBOX_STYLE || "ckc65ettm0ixc1ipexrpklr96"
-const mapboxUsername = process.env.MAPBOX_USERNAME || "ajinkyakulkarni"
+const dataBaseUrl = process.env.bamboo_DATA_BASE_URL || "https://ghrc-fcx-viz-output.s3-us-west-2.amazonaws.com"
+const abiBaseUrl = process.env.bamboo_ABI_BASE_URL || "https://q6eymnfsd9.execute-api.us-west-2.amazonaws.com"
+const flightTrackBaseUrl = process.env.bamboo_FLIGHT_TRACK_BASE_URL || "https://fcx-czml.s3.amazonaws.com"
+
+const mapboxAccessToken = process.env.bamboo_MAPBOX_ACCESS_TOKEN || "pk.eyJ1IjoiYWppbmt5YWt1bGthcm5pIiwiYSI6ImNrYzY1NHh3bDA3cXIyenA4dDdpOGlja2UifQ.g4r3pjugLK8yC2mkQmJr4w"
+const mapboxStyleId = process.env.bamboo_MAPBOX_STYLE || "ckc65ettm0ixc1ipexrpklr96"
+const mapboxUsername = process.env.bamboo_MAPBOX_USERNAME || "ajinkyakulkarni"
 const mapboxUrl = "https://api.mapbox.com/styles/v1/" + mapboxUsername + "/" + mapboxStyleId + "/tiles/256/{z}/{x}/{y}?access_token=" + mapboxAccessToken
 const cesiumDefaultAccessToken =
-  process.env.CESIUM_DEFAULT_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNmQyZDgxNS0xNzY4LTQ4MzctOWZkOC02YmZkYjQ1ZGI1MDUiLCJpZCI6ODU1MSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU5NDM4OTIxMn0.WPkQXxpVgW5AfQdZKB15dMnLxQIjmA_Q2qimnDXONvc"
-const supportEmail = process.env.GHRC_SUPPORT_EMAIL || "support-ghrc@earthdata.nasa.gov"
+  process.env.bamboo_CESIUM_DEFAULT_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNmQyZDgxNS0xNzY4LTQ4MzctOWZkOC02YmZkYjQ1ZGI1MDUiLCJpZCI6ODU1MSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU5NDM4OTIxMn0.WPkQXxpVgW5AfQdZKB15dMnLxQIjmA_Q2qimnDXONvc"
+const supportEmail = process.env.bamboo_GHRC_SUPPORT_EMAIL || "support-ghrc@earthdata.nasa.gov"
 
 export { dataBaseUrl, abiBaseUrl, flightTrackBaseUrl, mapboxUrl, cesiumDefaultAccessToken, supportEmail }
