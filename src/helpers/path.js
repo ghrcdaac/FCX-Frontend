e8f8b07388d39bb26273df6d1c8b031f590f3d79
Anupam Dahal <anupamdahal1@gmail.com>
2021-10-24 19:06:29 -0500

    Resolved non-basePath URL issue

--- src/helpers/path.js
+++ src/helpers/path.js
@@ -1,7 +1,10 @@
 import { basePath } from "../constants/enum"
 const checkPath = () => {
-  const path = new URL(window.location.href).pathname
-  return path === basePath
+  //TODO: Figure out how to check the correct path
+  
+  // const path = new URL(window.location.href).pathname
+  // return path === basePath
+  return true
 }
 
 export {checkPath}
