8e404413cd6b725cdfac7fc9ec55597f698b9229
Sanjog Thapa <sanzog03@gmail.com>
2022-11-18 00:56:46 -0600

    added toast for success and error messages. Hooked it with the axios api call, invoked by redux thunk for async actions

--- src/constants/thunk.js
+++ src/constants/thunk.js
@@ -1,5 +1,6 @@
 import APICall from "./ApiCall";
 import { subsettingApiKey } from "../config";
+import { toast } from 'react-toastify';
 
 const apiCaller = new APICall();
 apiCaller.setHeader(subsettingApiKey);
@@ -7,13 +8,15 @@ apiCaller.setHeader(subsettingApiKey);
 export const Get = Resources => {
   const {init, success, error} = Resources.asyncActions; // as actions for all the resource is same
   return (dispatch, getState) => {
-    dispatch(handleInit(init, undefined));
+    dispatch(initDispatchAction(init, undefined));
     apiCaller.get(Resources.url)
       .then(data => {
-        dispatch(handleSuccess(success, data));
+        handleSuccess(data.status, data.body);
+        dispatch(successDispatchAction(success, data));
       })
       .catch(err => {
-        dispatch(handleError(error, err));
+        handleError(err.status, "Something went wrong. Call Support.");
+        dispatch(errorDispatchAction(error, err));
       });
   };
 };
@@ -21,19 +24,50 @@ export const Get = Resources => {
 export const Post = Resources => {
   const {init, success, error} = Resources.asyncActions; // as actions for all the resource is same
   return (dispatch, getState) => {
-    dispatch(handleInit(init, undefined));
+    dispatch(initDispatchAction(init, undefined));
     apiCaller.post(Resources.url, Resources.body)
       .then(data => {
-        dispatch(handleSuccess(success, data));
+        handleSuccess(data.status, data.body);
+        dispatch(successDispatchAction(success, data));
       })
       .catch(err => {
-        dispatch(handleError(error, err));
+        handleError(400, "Something went wrong. Call Support.");
+        dispatch(errorDispatchAction(error, err));
       });
   };
 };
 
 // @utils
 
-const handleInit = (type, payload) => ({type, payload});
-const handleSuccess = (type, payload) => ({type, payload});
-const handleError = (type, payload) => ({type, payload});
+const initDispatchAction = (type, payload) => ({type, payload});
+const successDispatchAction = (type, payload) => ({type, payload});
+const errorDispatchAction = (type, payload) => ({type, payload});
+
+const handleSuccess = (status, body) => {
+  if (200 <= status < 300) {
+    toast.success('Subsetting invoke successful. It will take some time to finish subsetting.', {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      });
+  }
+  //TODO: add cases for 400, 500, 300, 100 status codes.
+}
+
+const handleError = (status, body) => {
+  toast.error(body, {
+    position: "bottom-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+}
