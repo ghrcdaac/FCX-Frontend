e1414a8eb0c3583b96a8f46d0c5878ec1db8f568
Sanjog <st0128@uah.edu>
2023-01-19 12:30:00 -0600

    API caller WIP

---
+++ src/components/instrumentsHistogram/helper/apiCaller.js
@@ -0,0 +1,14 @@
+/** 
+* Calls the api and fetches the data.
+* @summary API call to the histogram preprocessing lambda backend and receives data.
+* @param {instruments} parameterNameHere - Brief description of the parameter here. Note: For other notations of data types, please refer to JSDocs: DataTypes command.
+* @return {ReturnValueDataTypeHere} Brief description of the returning value here.
+*/
+
+function APICALL(URL) {
+   // call the api and fetch the data in appropriate format
+}
+
+function InstrumentsHandler(instrumentType, datetime, coordType, dataType, params, pageno, pagesize, density) {
+
+}
