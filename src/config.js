57a5cf3cd87124d8200baa59fac368d2097cda93
Sanjog <st0128@uah.edu>
2023-02-09 12:52:07 -0600

    added env for all new campaigns source

--- src/config.js
+++ src/config.js
@@ -9,6 +9,7 @@ const dataBaseUrl = process.env.REACT_APP_BAMBOO_DATA_BASE_URL
 
 const abiBaseUrl = process.env.REACT_APP_BAMBOO_ABI_BASE_URL 
 const flightTrackBaseUrl = process.env.REACT_APP_BAMBOO_FLIGHT_TRACK_BASE_URL 
+const newFieldCampaignsBaseUrl = process.env.REACT_APP_NEW_FIELD_CAMPAIGNS_BASE_URL
 
 const mapboxAccessToken = process.env.REACT_APP_BAMBOO_MAPBOX_ACCESS_TOKEN 
 const mapboxStyleId = process.env.REACT_APP_BAMBOO_MAPBOX_STYLE 
@@ -17,7 +18,7 @@ const mapboxUrl = "https://api.mapbox.com/styles/v1/" + mapboxUsername + "/" + m
 const cesiumDefaultAccessToken = process.env.REACT_APP_BAMBOO_CESIUM_DEFAULT_TOKEN 
 const supportEmail = process.env.REACT_APP_BAMBOO_GHRC_SUPPORT_EMAIL || "support-ghrc@earthdata.nasa.gov"
 
-export { dataBaseUrl, abiBaseUrl, flightTrackBaseUrl, mapboxUrl, cesiumDefaultAccessToken, supportEmail }
+export { dataBaseUrl, abiBaseUrl, flightTrackBaseUrl, mapboxUrl, cesiumDefaultAccessToken, supportEmail, newFieldCampaignsBaseUrl }
 
 /*
   dataBaseUrl -  (S3) URL root address where the data resides
