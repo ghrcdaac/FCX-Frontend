e11d92cfb22218bcd92218269b70284cfb013f98
Sanjog <st0128@uah.edu>
2023-02-10 11:38:27 -0600

    display flight path for all the available data

--- src/layers/olympex-layers/components/instrumentLayers/index.js
+++ src/layers/olympex-layers/components/instrumentLayers/index.js
@@ -1,6 +1,5 @@
 import { flighttrack } from "./helpers";
 
-const dates = ["2015-11-10"];
 const instruments = ["trackEr2", "trackDc8"]
 // const instruments = ["trackDc8"]
 
@@ -14,8 +13,10 @@ function getInstrumentsItem(date, instrumentType, index) {
     */
     switch(instrumentType) {
         case "trackEr2":
+            if (!er2_dates.includes(date)) return null;
             return flighttrack(date, "er2", index);
         case "trackDc8":
+            if (!dc8_dates.includes(date)) return null;
             return flighttrack(date, "dc8", index);
         default:
             return null
@@ -28,10 +29,14 @@ function getInstrumentsItem(date, instrumentType, index) {
  * Returns the object with date and list of items (instrument items data for visualization)
 */
 
-const instrumentLayers = dates.map(date => ({
-        date: date,
-        items: instruments.map((instrum, index) => getInstrumentsItem(date, instrum, index))
+let dc8_dates = ["2015-11-05", "2015-11-10", "2015-11-12", "2015-11-13", "2015-11-14", "2015-11-19", "2015-11-23", "2015-11-24", "2015-11-25", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-08", "2015-12-10", "2015-12-10", "2015-12-12", "2015-12-13", "2015-12-18", "2015-12-19", "2015-12-19"]
+let er2_dates = ["2015-11-09", "2015-11-10", "2015-11-17", "2015-11-19", "2015-11-23", "2015-11-24", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-09", "2015-12-10", "2015-12-12", "2015-12-14", "2015-12-15"]
+
+var unique_dates = [...dc8_dates, ...er2_dates].filter((value, index, array) => array.indexOf(value) === index);
+
+const instrumentLayers = unique_dates.map(date => ({
+        date,
+        items: instruments.map((instrum, index) => getInstrumentsItem(date, instrum, index)).filter(n => n)
     }));
-// TODO: filter any null values.
 
 export default instrumentLayers;
