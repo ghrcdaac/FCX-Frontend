b64e32c7a6337ff77b0ca55c524fe88d09e22f63
Sanjog <st0128@uah.edu>
2023-02-14 09:35:02 -0600

    renamed for uniformity and removed unnecessary files

--- src/layers/olympex-layers/components/index.js
+++ src/layers/olympex-layers/components/index.js
@@ -1,4 +1,4 @@
-import {campaign, logo, description, dois, links, legends, defaultCamera} from "./constant";
+import {campaign, logo, description, dois, links, legends, defaultCamera} from "./constantLayers";
 import instrumentLayers from "./instrumentLayers";
 
 export {campaign, logo, description, dois, links, legends, defaultCamera, instrumentLayers}
