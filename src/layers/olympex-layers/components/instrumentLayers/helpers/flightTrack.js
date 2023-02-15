d9a996c248b24975dea4fbe21bace1b923db913a
Sanjog <st0128@uah.edu>
2023-02-15 10:19:34 -0600

    changes in layer generator class

--- src/layers/olympex-layers/components/instrumentLayers/helpers/flightTrack.js
+++ src/layers/olympex-layers/components/instrumentLayers/helpers/flightTrack.js
@@ -3,14 +3,12 @@ import { newFieldCampaignsBaseUrl } from "../../../../../config"
 export default function flighttrack (date, aircraft, index) {
     /** 
     * returns structured flight meta item.
-    * @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
     * @param {date} string - YYYY-MM-DD format. Date of the campaign when the flight took off.
     * @param {aircraft} string - Type of aircraft used in the field campaign.
     * @param {index} number (Optional) - Index of the instrument in the list of instruments that are visualization wanted
     * @return {Object} structured flight meta item.
     */
     
-    // TODO: how to get start and end time of the campaign for a particular date.????
     if (aircraft == "er2") {
         return {
             layerId: `${date}-track${index}-er2`,
--- src/layers/olympex-layers/components/instrumentLayers/index.js
+++ src/layers/olympex-layers/components/instrumentLayers/index.js
@@ -1,16 +1,18 @@
 import { flighttrack } from "./helpers";
 
 class LayerGenerator {
-    constructor(instruments) {
-        this.uniqueDates = []
-        this.instruments = instruments;
+    constructor() {
+        this.instruments = ["trackEr2", "trackDc8"]
+        this.dc8_dates = ["2015-11-05", "2015-11-10", "2015-11-12", "2015-11-13", "2015-11-14", "2015-11-19", "2015-11-23", "2015-11-24", "2015-11-25", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-08", "2015-12-10", "2015-12-12", "2015-12-18", "2015-12-19"];
+        this.er2_dates = ["2015-11-09", "2015-11-10", "2015-11-17", "2015-11-19", "2015-11-23", "2015-11-24", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-09", "2015-12-10", "2015-12-12", "2015-12-14", "2015-12-15"];
+        this.crs_dates = []
     }
 
-    addDates(dates) {
+    uniqueDates(date) {
         /**
         * @param {array} dates array of dates.
         */
-        this.uniqueDates = [...this.uniqueDates, ...dates].filter((value, index, array) => array.indexOf(value) === index);
+        return [...date].filter((value, index, array) => array.indexOf(value) === index);
     }
 
     getInstrumentsItem(date, instrumentType, index) {
@@ -23,10 +25,10 @@ class LayerGenerator {
         */
         switch (instrumentType) {
             case "trackEr2":
-                if (!er2_dates.includes(date)) return null; //TODO: later take er2_dates when loaded through addDates.
+                if (!this.er2_dates.includes(date)) return null; //TODO: later take er2_dates when loaded through addDates.
                 return flighttrack(date, "er2", index);
             case "trackDc8":
-                if (!dc8_dates.includes(date)) return null;
+                if (!this.dc8_dates.includes(date)) return null;
                 return flighttrack(date, "dc8", index);
             default:
                 return null
@@ -37,21 +39,14 @@ class LayerGenerator {
         /**
         * @return {object} A structured instruments layer.
         */
-        return this.uniqueDates.map(date => ({
+        return this.uniqueDates([...this.dc8_dates, ...this.er2_dates]).map(date => ({
             date,
             items: this.instruments.map((instrum, index) => this.getInstrumentsItem(date, instrum, index)).filter(n => n)
         }));
     }
 }
 
-
-let instruments = ["trackEr2", "trackDc8"]
-let dc8_dates = ["2015-11-05", "2015-11-10", "2015-11-12", "2015-11-13", "2015-11-14", "2015-11-19", "2015-11-23", "2015-11-24", "2015-11-25", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-08", "2015-12-10", "2015-12-12", "2015-12-18", "2015-12-19"];
-let er2_dates = ["2015-11-09", "2015-11-10", "2015-11-17", "2015-11-19", "2015-11-23", "2015-11-24", "2015-12-02", "2015-12-03", "2015-12-04", "2015-12-05", "2015-12-09", "2015-12-10", "2015-12-12", "2015-12-14", "2015-12-15"];
-
-let layersGen = new LayerGenerator(instruments);
-layersGen.addDates(dc8_dates);
-layersGen.addDates(er2_dates);
+let layersGen = new LayerGenerator();
 let instrumentLayers = layersGen.generateLayer();
 
 export default instrumentLayers;
