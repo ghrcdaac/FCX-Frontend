cd06b397a5269ce8539b567de47d8e6582cc3fa7
Sanjog Thapa <sanzog03@gmail.com>
2023-04-02 23:59:53 -0500

    added code to visualize multiple npol 3d point clouds

--- src/layers/olympex-layers/components/instrumentLayers/helpers/npol.js
+++ src/layers/olympex-layers/components/instrumentLayers/helpers/npol.js
@@ -1,10 +1,11 @@
 import { newFieldCampaignsBaseUrl } from "../../../../../config"
 
-export default function npol (date, index) {
+export default function npol (date, index, dataPoints) {
     /** 
     * returns structured npol meta item.
     * @param {date} string - YYYY-MM-DD format. Date of the campaign when the flight took off that collected npol data.
-    * @param {index} number (Optional) - Index of the instrument in the list of instruments that are visualization wanted
+    * @param {index} number - Index of the instrument in the list of instruments that are visualization wanted
+    * @param {dataPoints} number - represents the number of 3dtile data available for the given date.
     * @return {Object} structured npol meta item.
     */
 
@@ -18,8 +19,9 @@ export default function npol (date, index) {
         unit: "dBZ",
         // for each data available accross each date, create a list of tile locations. 
         // tileLocation: [`${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/${date.replace(/-/g,'')}/tileset.json`],
-        tileLocation: [ `${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/20151203b/tileset.json`,
-                        `${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/20151203/tileset.json` ],
+        // tileLocation: [ `${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/20151203b/tileset.json`,
+        //                 `${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/20151203/tileset.json` ],
+        tileLocation: [...Array(dataPoints-1).keys()].map(idx => `${newFieldCampaignsBaseUrl}/Olympex/instrument-processed-data/npol/${date.replace(/-/g,'')}/freq-${idx}/tileset.json`),
         date,
         type: "instrument",
         platform: "ground",
