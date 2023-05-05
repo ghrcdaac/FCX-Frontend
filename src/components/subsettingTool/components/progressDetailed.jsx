4521f16389a3d99d69b284c129ea2a205fdc8cad
Sanjog <st0128@uah.edu>
2023-05-05 12:59:22 -0500

    changes to logic for showing progress message

--- src/components/subsettingTool/components/progressDetailed.jsx
+++ src/components/subsettingTool/components/progressDetailed.jsx
@@ -3,17 +3,37 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 
+const refProgress = {
+                    "fegs": "false",
+                    "lis": "false",
+                    "glm": "false",
+                    "lip": "false",
+                    "lma": "false",
+                    "crs": "false"
+                    }
+
 export default function DetailedProgressBar(props) {
-    // {progress && progress.map((elem, index) => (<p key={`${index}-${elem['wstokenid']}`}>{elem.message}</p>))}
+    if (props.progress) props.progress.forEach(instrProgress => {
+        let {wstokenid, message, ...rest} = instrProgress;
+        for (let [key, value] of Object.entries(rest)) { // always 1 execution for a object with single value.
+            refProgress[`${key}`] = value;
+        }
+    });
+
     return (
     <Box>
         <Box sx={{ width: '100%' }}>
             <LinearProgressWithLabel value={props.progressPercentage} />
         </Box>
         <Paper>
-            <div style={{margin: "1rem", padding: "1rem"}}>
-                {props.progress && props.progress.map((elem, index) => (<Typography variant="h6" key={`${index}-${elem['wstokenid']}`}>{elem.message}</Typography>))}
-            </div>
+            <Typography variant="body2" style={{margin: "1rem", padding: "1rem"}}>
+                Subsetting CRS  : {(refProgress["crs"] === "true") ? "Complete" : "Progress"} <br/>
+                Subsetting FEGS : {(refProgress["fegs"] === "true") ? "Complete" : "Progress"} <br/>
+                Subsetting GLM  : {(refProgress["glm"] === "true") ? "Complete" : "Progress"} <br/>
+                Subsetting LIS  : {(refProgress["lis"] === "true") ? "Complete" : "Progress"} <br/>
+                Subsetting LIP  : {(refProgress["lip"] === "true") ? "Complete" : "Progress"} <br/>
+                Subsetting LMA  : {(refProgress["lma"] === "true") ? "Complete" : "Progress"} <br/>
+            </Typography>
         </Paper>
     </Box>
     );
